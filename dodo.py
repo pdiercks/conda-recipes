b1b9df358ac4a3c9a526589e261b830a837290b9
pdiercks <philipp.diercks@bam.de>
2021-09-02 15:24:06 +0200

    add betterbib; add uptodate; fix target suffix

--- dodo.py
+++ dodo.py
@@ -1,10 +1,12 @@
 import pathlib
+from doit.tools import run_once
 
 ROOT = pathlib.Path(__file__).parent
 PACKAGES = {
     "pypi": {
         "pygmsh": "6.1.1",
         "neovim-remote": "2.4.0",
+        "betterbib": "4.0.0",
     }
 }
 
@@ -15,6 +17,7 @@ def task_pypi_packages():
         yield {
             "name": package,
             "actions": [f"grayskull pypi {package}=={version}"],
-            "targets": [ROOT / package / "meta.yml"],
+            "targets": [ROOT / package / "meta.yaml"],
+            "uptodate": [run_once],
             "clean": True,
         }
